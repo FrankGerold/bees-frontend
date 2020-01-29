bc6ff75640815669c227a8ad8a00f87ed2da67e6
Frank Gerold <FrankETSGerold@gmail.com>
2020-01-29 17:03:09 -0500

    new branch for testing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ayyy lmao
         </p>
         <a
           className="App-link"
