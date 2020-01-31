3cf5493eb926e442a437a6b6159b77eba0c6daa5
Frank Gerold <FrankETSGerold@gmail.com>
2020-01-31 17:12:36 -0500

    Signup form? might refactor

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,7 +1,73 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useDispatch} from 'react-redux';
+import Actions from '../Redux/actions.js';
 
-const Signup = ({}) => (
-  <div>Signup</div>
-);
+const Signup = ({history}) => {
+
+  const dispatch = useDispatch();
+
+  const {signupForm, setSignupForm} = useState({
+    name: '',
+    userName: '',
+    password: '',
+    bio:'',
+    image:''
+  })
+
+  const handleChange = (typing) => {
+    setSignupForm({
+      ...signupForm,
+      typing.target.name: typing.target.value
+    })
+  };
+
+  const handleSubmit = (form) => {
+    form.preventDefault();
+    dispatch(Actions.newUser(signupForm));
+    history.puch('/');
+  };
+
+  const {name, userName, password, bio, image} = signupForm;
+
+  return (
+    <form onSumbit={handleSubmit}>
+      <input
+        type='text'
+        name='name'
+        value={name}
+        onChange={handleChange}
+        placeholder='Name'
+      />
+      <input
+        type='text'
+        name='userName'
+        value={userName}
+        onChange={handleChange}
+        placeholder='Username'
+      />
+      <input
+        type='password'
+        name='password'
+        value={password}
+        onChange={handleChange}
+        placeholder='Password'
+      />
+      <input
+        type='text'
+        name='bio'
+        value={bio}
+        onChange={handleChange}
+        placeholder='Bio'
+      />
+      <input
+        type='text'
+        name='image'
+        value={image}
+        onChange={handleChange}
+        placeholder='Image URL'
+      />
+    </form>
+  )
+}
 
 export default Signup;
